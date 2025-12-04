65b93651230c6753b39de71dac96eb8d8d12c3f8
JK <viletert@gmail.com>
2025-12-05 00:15:09 +0200

    Turn off data fetch during build

--- server/product.tsx
+++ server/product.tsx
@@ -4,7 +4,7 @@ import {Product} from "@/components/shared/types";
 
 export async function getProduct(id: number): Promise<Product> {
     try {
-        const res = await fetch(`https://fakestoreapi.com/products/${id}`);
+        const res = await fetch(`https://fakestoreapi.com/products/${id}`, {cache: "no-store"});
         return await res.json();
     } catch (error) {
         console.error('Error fetching the product from server', error);
--- server/products.tsx
+++ server/products.tsx
@@ -4,7 +4,7 @@ import {Product} from "@/components/shared/types";
 
 export async function getProducts(): Promise<Product[]> {
     try {
-        const res = await fetch("https://fakestoreapi.com/products");
+        const res = await fetch("https://fakestoreapi.com/products", {cache: "no-store"});
         return await res.json();
     } catch (error) {
         console.error("Error fetching the products list from server", error);
